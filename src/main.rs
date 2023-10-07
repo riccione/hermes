bfcf9dded8b604c651d4908044a1f82a95984733
Zero <zero@zero-ms7c56.ignes-zero.com>
2023-10-07 11:27:41 -0400

    Remove get fn Add additional field to codex
    I've introduced a new field in the codex file. At the end of each line,
    I append either '0' to indicate no encryption or '1' to signify encryption,
    with the latter being the case when a password is set. This adjustment
    effectively resolves the problem I previously addressed in a commit.
    
    Additionally, I've made another change that relates to the code used for the
    'get' and 'ls' functions. To streamline the codebase, I decided to eliminate
    the 'get' function entirely and retain only the 'ls' function. This revised
    'ls' function now accepts an optional 'String' argument for an alias, allowing
    me to handle both scenarios within a single function.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+
 use std::time::{SystemTime, UNIX_EPOCH};
 use totp_lite::{totp_custom, Sha1, DEFAULT_STEP};
 use koibumi_base32 as base32;
@@ -51,15 +52,10 @@ enum Commands {
         #[clap(short = 'p', long)]
         password: bool,
     },
-    /// Get code by alias
-    Get {
+    /// Get codes for all/alias records
+    Ls {
         #[clap(short = 'a', long)]
         alias: Option<String>,
-        #[clap(short = 'p', long)]
-        password: bool,
-    },
-    /// Get codes for all records
-    Ls {
         #[clap(short = 'p', long)]
         password: bool,
     },
@@ -80,8 +76,10 @@ fn main() {
                     } else {
                         code.as_ref().unwrap().to_string()
                     };
+                    let is_encrypted = *password as u8;
                     add(alias.as_ref().unwrap().as_str(), 
-                        pass.as_str());
+                        pass.as_str(),
+                        &is_encrypted);
                 } else {
                     println!("Don't use : in alias or code'");
                     std::process::exit(1);
@@ -108,28 +106,21 @@ fn main() {
                 } else {
                     code.as_ref().unwrap().to_string()
                 };
+                let is_encrypted = *password as u8;
                 update_code(alias.as_ref().unwrap().as_str(), 
-                    pass.as_str());
+                    pass.as_str(),
+                    &is_encrypted);
             }
         },
-        Commands::Get { alias, password } => {
-            if alias.is_some() {
-                get(alias.as_ref().unwrap().as_str(),
-                    password);
-            } else {
-                println!("Error: no arguments for get command");
-                std::process::exit(1);
-            }
-        },
-        Commands::Ls { password } => {
-            ls(password);
+        Commands::Ls { alias, password } => {
+            ls(alias, password);
         }
     };
 }
 
-fn add(alias: &str, code: &str) {
+fn add(alias: &str, code: &str, is_encrypted: &u8) {
     // create a storage file if it does not exist
-    let data = format!("{}:{}\n", alias, code);
+    let data = format!("{}:{}:{}\n", alias, code, is_encrypted);
     
     if file_exists() {
         // check if alias already exists and return error message
@@ -151,9 +142,9 @@ fn add(alias: &str, code: &str) {
     println!("{otp}");
 }
 
-fn update_code(alias: &str, code: &str) {
+fn update_code(alias: &str, code: &str, is_encrypted: &u8) {
     remove(&alias);
-    add(&alias, &code);
+    add(&alias, &code, &is_encrypted);
 }
 
 fn remove(alias: &str) {
@@ -168,46 +159,48 @@ fn remove(alias: &str) {
     write_to_file(&data);
 }
 
-fn get(alias: &str, is_encrypted: &bool) {
-    let lines = read_file_to_vec();
-    for l in lines {
-        let x: Vec<&str> = l.split(DELIMETER).collect();
-        // alias exists in the vector
-        if x[0] == alias {
-            let code = if *is_encrypted {
-                crypt(false,
-                    &x[1].to_string(), 
-                    &input_password())
-            } else {
-                x[1].to_string()
-            };
-        let otp = generate_otp(code.as_str());
-        println!("{otp}");
-        }
-    }
+fn get(is_password: &bool, is_encrypted: &str, pass: &String, x: &str) -> String {
+    let code = if *is_password && is_encrypted == "1" {
+        crypt(false,
+            &x.to_string(),
+            &pass)
+    } else {
+        x.to_string()
+    };
+    let otp = if !*is_password && is_encrypted == "1" {
+        "Cannot decrypt - provide a password".to_string()
+    } else {
+        generate_otp(code.as_str())
+    };
+    otp
 }
 
-fn ls(is_encrypted: &bool) {
-    let pass = if *is_encrypted {
+fn ls(alias: &Option<String>, is_password: &bool) {
+    let pass = if *is_password {
         input_password()
     } else {
         "".to_string()
     };
     let lines = read_file_to_vec();
-    println!("Alias\tOTP");
+    if alias.is_none() {
+        println!("Alias\tOTP");
+    }
     for l in lines {
         let x: Vec<&str> = l.split(DELIMETER).collect();
-        let alias = x[0];
-        let code = if *is_encrypted {
-            crypt(false,
-                &x[1].to_string(),
-                &pass)
+        let alias_curr = x[0];
+        let is_encrypted = x[2];
+        if alias.is_some() {
+            if alias.as_ref().unwrap() == alias_curr {
+                let otp = get(&is_password, &is_encrypted, &pass, &x[1]);
+                println!("{otp}");
+                break;
+            }
         } else {
-            x[1].to_string()
-        };
-        let otp = generate_otp(code.as_str());
-        println!("{alias}\t{otp}");
+            let otp = get(&is_password, &is_encrypted, &pass, &x[1]);
+            println!("{alias_curr}\t{otp}");
+        }
     }
+    std::process::exit(0);
 }
 
 fn read_file_to_vec() -> Vec<String> {
@@ -273,6 +266,7 @@ fn generate_otp(x: &str) -> String {
 
 fn input_password() -> String {
     let mut input = String::new();
+    println!("Enter password: ");
     let stdin = io::stdin();
     stdin.read_line(&mut input).expect("Could not read password");
     input
@@ -287,7 +281,6 @@ fn crypt(encrypt: bool, code: &String, password: &str) -> String {
             mcrypt
             .encrypt_str_to_base64(code)
     } else {
-        //let base64 = base32::decode(code.as_bytes());
         let decrypted = match mcrypt.decrypt_base64_to_string(code) {
             Ok(decrypted) => decrypted,
             Err(_) => TALARIA.to_string(),
