792e71aa91a067b110fed2937bfe6f45aa497d2a
riccione <28346232+riccione@users.noreply.github.com>
2025-12-31 09:54:37 +0700

    feat: add constructor logic and timestamp generation for Record #17

--- src/cmd.rs
+++ src/cmd.rs
@@ -3,6 +3,7 @@ use crate::otp;
 use data_encoding::BASE32_NOPAD;
 use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 const DELIMETER: &str = ":";
 
@@ -13,6 +14,24 @@ pub struct Record {
     pub is_unencrypted: bool, // only for DEBUG, store secret unencrypted
     pub algorithm: String,
     pub created_at: u64, // Unix timestamp in sec
+}
+
+impl Record {
+    pub fn new(alias: String, secret: String, is_unencrypted: bool) -> Self {
+        let start = SystemTime::now();
+        let since_the_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+
+        Self {
+            alias,
+            secret,
+            is_unencrypted,
+            algorithm: "sha1".to_string(),
+            created_at: since_the_epoch,
+        }
+    }
 }
 
 fn input_password() -> String {
