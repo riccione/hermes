c0f742a9d0b5bc412f10e18b5e484d7fa465f2b8
riccione <28346232+riccione@users.noreply.github.com>
2026-01-03 11:31:56 +0700

    refactor: restructure CLI args
    - Move all Clap structs and enums to `args.rs` for better separation.

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,71 @@
-use clap::ValueEnum;
+use clap::{Parser, Subcommand, ValueEnum};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)] // Read from Cargo.toml
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
 
 #[derive(ValueEnum, Clone, Debug, Default)]
 pub enum OutputFormat {
     #[default]
     Table,
     Json,
+}
+
+#[derive(clap::Args)]
+pub struct EncryptArgs {
+    /// WARNING: Store the secret in plain text. Use for debugging only.
+    #[clap(short = 'u', long, verbatim_doc_comment)]
+    pub unencrypt: bool,
+    /// WARNING: Using this flag leaves password in shell history.
+    #[clap(short = 'p', long, verbatim_doc_comment)]
+    pub password: Option<String>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Adds code to the hermes
+    Add {
+        #[clap(short = 'a', long)]
+        alias: String,
+        #[clap(short = 'c', long)]
+        code: String,
+        #[clap(flatten)]
+        encryption: EncryptArgs,
+    },
+    /// Remove code from the hermes
+    Remove {
+        #[clap(short = 'a', long)]
+        alias: String,
+    },
+    /// Update code by alias
+    Update {
+        #[clap(short = 'a', long)]
+        alias: String,
+        #[clap(short = 'c', long)]
+        code: String,
+        #[clap(flatten)]
+        encryption: EncryptArgs,
+    },
+    /// Rename alias
+    Rename {
+        old_alias: String,
+        new_alias: String,
+    },
+    /// Get codes for all/alias records
+    Ls {
+        #[clap(short = 'a', long)]
+        alias: Option<String>,
+        #[arg(short = 'f', long, value_enum, default_value_t = OutputFormat::Table)]
+        format: OutputFormat,
+        #[clap(flatten)]
+        encryption: EncryptArgs,
+    },
+    /// Show location of codex file
+    Config {},
+    /// Migrate legacy codex format to JSON
+    Migrate,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 mod args;
@@ -7,82 +6,21 @@ mod file;
 mod models;
 mod otp;
 
-use crate::args::OutputFormat;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)] // Read from Cargo.toml
-struct Args {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(clap::Args)]
-struct EncryptArgs {
-    /// WARNING: Store the secret in plain text. Use for debugging only.
-    #[clap(short = 'u', long, verbatim_doc_comment)]
-    unencrypt: bool,
-    /// WARNING: Using this flag leaves password in shell history.
-    #[clap(short = 'p', long, verbatim_doc_comment)]
-    password: Option<String>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Adds code to the hermes
-    Add {
-        #[clap(short = 'a', long)]
-        alias: String,
-        #[clap(short = 'c', long)]
-        code: String,
-        #[clap(flatten)]
-        encryption: EncryptArgs,
-    },
-    /// Remove code from the hermes
-    Remove {
-        #[clap(short = 'a', long)]
-        alias: String,
-    },
-    /// Update code by alias
-    Update {
-        #[clap(short = 'a', long)]
-        alias: String,
-        #[clap(short = 'c', long)]
-        code: String,
-        #[clap(flatten)]
-        encryption: EncryptArgs,
-    },
-    /// Rename alias
-    Rename {
-        old_alias: String,
-        new_alias: String,
-    },
-    /// Get codes for all/alias records
-    Ls {
-        #[clap(short = 'a', long)]
-        alias: Option<String>,
-        #[arg(short = 'f', long, value_enum, default_value_t = OutputFormat::Table)]
-        format: OutputFormat,
-        #[clap(flatten)]
-        encryption: EncryptArgs,
-    },
-    /// Show location of codex file
-    Config {},
-    /// Migrate legacy codex format to JSON
-    Migrate,
-}
+use args::{Cli, Commands};
+use clap::Parser;
 
 fn main() {
     let codex_path: PathBuf = file::get_codex_path();
-    let args = Args::parse();
+    let cli = Cli::parse();
 
-    if let Err(e) = run(args, codex_path) {
+    if let Err(e) = run(cli.command, codex_path) {
         eprintln!("Error: {e}");
         std::process::exit(1);
     }
 }
 
-fn run(args: Args, codex_path: PathBuf) -> Result<(), String> {
-    match &args.command {
+fn run(command: Commands, codex_path: PathBuf) -> Result<(), String> {
+    match command {
         Commands::Add {
             alias,
             code,
@@ -132,10 +70,10 @@ fn run(args: Args, codex_path: PathBuf) -> Result<(), String> {
         } => {
             cmd::ls(
                 &codex_path,
-                alias,
+                &alias,
                 &encryption.unencrypt,
                 &encryption.password,
-                format,
+                &format,
             );
         }
         Commands::Config {} => {
